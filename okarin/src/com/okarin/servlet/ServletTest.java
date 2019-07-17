6a1067181678a2938f861a089a79fefb0357567f
phaple <276348586@qq.com>
2019-07-17 11:55:43 +0800

    okarin->servlet & struts

---
+++ okarin/src/com/okarin/servlet/ServletTest.java
@@ -0,0 +1,19 @@
+package com.okarin.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServletTest extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("getting...!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
